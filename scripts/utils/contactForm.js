418b32050a3938853b3635d000f49e6dc69b76f8
bintou <fofana_bintou@outlook.fr>
2022-07-18 13:39:36 +0200

    Affichage du message dans la console

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -13,5 +13,5 @@ function console_log_infos(){
     console.log(document.querySelector("input[name='prenom']").value);
     console.log(document.querySelector("input[name='nom']").value);
     console.log(document.querySelector("input[name='email']").value);
-    console.log(document.querySelector("input[name='message']").value);
+    console.log(document.querySelector("textarea[name='message']").value);
 }
